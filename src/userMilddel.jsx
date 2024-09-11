705625a34a0a9bda536eb8d83a3b5ffbfe6957d3
wassim <118565668+wassim-sissimo@users.noreply.github.com>
2024-09-11 01:21:28 +0100

    v0.0.0.6
    fixed a path error

--- src/userMilddel.jsx
+++ src/userMilddel.jsx
@@ -1,15 +1,15 @@
 import { useChatStore } from "./useChatStor"
-
-import phone from "../assets/imgs/phone.png"
-import video from "../assets/imgs/video.png"
-import search from "../assets/imgs/search.png"
+import avatar from "./assets/imgs/avatar.png"
+import phone from "./assets/imgs/phone.png"
+import video from "./assets/imgs/video.png"
+import search from "./assets/imgs/search.png"
 
 export const UserMiddel=()=>{
     const {user}=useChatStore()
     return(
         <div className="py-3 border-b border-bColor px-2 flex justify-between">
             <div className="flex space-x-4">
-                <img src={user?.profilePicUrl || "./src/assets/imgs/avatar.png"} alt=""  className="w-9 h-9 object-cover rounded-full"/>
+                <img src={user?.profilePicUrl || avatar} alt=""  className="w-9 h-9 object-cover rounded-full"/>
                 <div> 
                     <p className="text-white font-semibold">{user?.userName}</p>
                     <p className="text-white font-light text-[13px]">Lorem ipsum, dolor sit</p>
